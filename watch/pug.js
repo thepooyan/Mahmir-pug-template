5b3ac094ad62d8c453db7ab606ce240095642fb8
Pooyan <thepooyan@gmail.com>
2022-11-10 14:31:42 +0330

    getting hang of dynamic nesting

--- watch/pug.js
+++ watch/pug.js
@@ -3,6 +3,9 @@ const fs = require('fs');
 const path = require('path');
 
 function compile() {
+    let file = fs.readFileSync('layouts/layout1.pug', 'utf-8');
+    let addChild = file.replace('#{child}', 'include ../pages/edit');
+    fs.writeFileSync('layouts/layout1.pug', addChild, 'utf-8');
     const compiler = pug.compileFile('index.pug');
     let rse = compiler()
     fs.writeFileSync('./0Export/index.html', rse)
