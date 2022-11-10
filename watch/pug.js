94ae5768d1aa7e0c645e09f9aea1e3a846358e3e
Pooyan <thepooyan@gmail.com>
2022-11-10 14:46:21 +0330

    the new template is done at stage one

--- watch/pug.js
+++ watch/pug.js
@@ -3,19 +3,24 @@ const fs = require('fs');
 const path = require('path');
 
 function compile() {
-    let file = fs.readFileSync('layouts/layout1.pug', 'utf-8');
-    let addChild = file.replace('#{child}', 'include ../pages/edit');
-    fs.writeFileSync('layouts/layout1.pug', addChild, 'utf-8');
-    const compiler = pug.compileFile('index.pug');
-    let rse = compiler()
-    fs.writeFileSync('./0Export/index.html', rse)
+    const pages = fs.readdirSync('layouts/pages','utf-8');
+    pages.forEach(item=>{
+        console.log('compiling...')
+        let name = path.parse(item).name;
+        let file = fs.readFileSync('layouts/layout1.pug', 'utf-8');
+        let addChild = file.replace(/include .*/, `include pages/${name}`);
+        fs.writeFileSync('layouts/layout1.pug', addChild, 'utf-8');
+        let compiler = pug.compileFile('index.pug');
+        let rse = compiler()
+        fs.writeFileSync(`./0Export/${name}.html`, rse)
+    })
 }
 compile()
 
 //watch depencies
-const dir = fs.readdirSync('components', {encoding: 'utf-8'});
+const dir = fs.readdirSync('layouts/pages/', {encoding: 'utf-8'});
 dir.forEach(item=>{
     if (path.extname(item).toLowerCase() === '.pug')
-    fs.watch(`components/${item}`, 'utf-8', compile)
+    fs.watch(`layouts/pages/${item}`, 'utf-8', compile)
 })
 
