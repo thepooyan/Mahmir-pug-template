8b3ec1e11cf0826ef510b7a271569006f77bc026
Pooyan <thepooyan@gmail.com>
2022-11-10 10:25:51 +0330

    making the template

--- watch/pug.js
+++ watch/pug.js
@@ -1,8 +1,18 @@
 const pug = require('pug');
 const fs = require('fs');
+const path = require('path');
 
-const compiler = pug.compileFile('index.pug');
+function compile() {
+    const compiler = pug.compileFile('index.pug');
+    let rse = compiler()
+    fs.writeFileSync('./0Export/index.html', rse)
+}
+compile()
 
-let rse = compiler()
+//watch depencies
+const dir = fs.readdirSync('components', {encoding: 'utf-8'});
+dir.forEach(item=>{
+    if (path.extname(item).toLowerCase() === '.pug')
+    fs.watch(`components/${item}`, 'utf-8', compile)
+})
 
-fs.writeFileSync('./0Export/index.html', rse)
