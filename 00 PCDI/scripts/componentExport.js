0a38b913446da6e4a0d005b7afbc82e024caf290
pooyan <thepooyan@gmail.com>
2022-11-16 23:11:05 +0330

    halfway through componental export

--- 00 PCDI/scripts/componentExport.js
+++ 00 PCDI/scripts/componentExport.js
@@ -1,1 +1,23 @@
-// to be built...
+const child = process.argv[2];
+const fs = require('fs');
+const path = require('path');
+
+
+function findTrg(trgName) {
+    let target;
+    
+    const pages = fs.readdirSync('pages');
+    const innerPagesNames = pages.filter(i=>path.parse(i).ext==='')
+    
+    innerPagesNames.forEach(i => {
+        const content = fs.readdirSync(`pages/${i}`);
+        let res = content.find(i=>path.parse(i).base === trgName)
+        if (res)
+        target = res;
+        
+    });
+
+    return target
+}
+
+console.log(findTrg(process.argv[2]));
