08b98a10de1bf6f817273fb0016c209d13d1d452
Pooyan <thepooyan@gmail.com>
2022-11-18 13:44:27 +0330

    static export complete

--- 00 PCDI/scripts/componentExport.js
+++ 00 PCDI/scripts/componentExport.js
@@ -1,6 +1,7 @@
 const child = process.argv[2];
 const fs = require('fs');
 const path = require('path');
+const pug = require('pug')
 
 
 function findTrg(trgName) {
@@ -9,15 +10,22 @@ function findTrg(trgName) {
     const pages = fs.readdirSync('pages');
     const innerPagesNames = pages.filter(i=>path.parse(i).ext==='')
     
-    innerPagesNames.forEach(i => {
-        const content = fs.readdirSync(`pages/${i}`);
-        let res = content.find(i=>path.parse(i).base === trgName)
-        if (res)
-        target = res;
-        
+    innerPagesNames.forEach(inner => {
+        const content = fs.readdirSync(`pages/${inner}`);
+        let res = content.find(i=>path.parse(i).name === trgName && path.parse(i).ext.toLowerCase() === '.pug')
+        if (res) {
+            target = `./pages/${inner}/${res}`;
+        }
     });
 
     return target
 }
 
-console.log(findTrg(process.argv[2]));
+let found = findTrg(child)
+let compiler = pug.compileFile(found, {pretty: true});
+let compiledFile = compiler()
+
+fs.mkdirSync('0StaticExport', {recursive: true})
+fs.writeFileSync(`0StaticExport/${child}.html`, compiledFile, 'utf-8');
+
+console.log(found);
