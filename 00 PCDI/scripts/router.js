180e0c26b90bc35f8813a75ae94298291b37ba16
pooyan <thepooyan@gmail.com>
2022-11-19 18:55:04 +0330

    compiling child

--- 00 PCDI/scripts/router.js
+++ 00 PCDI/scripts/router.js
@@ -11,6 +11,7 @@ function cleanVD() {
         console.log(err);
     }
 }
+
 function buildBase(page, isChild=false) {
     //build the base for the page
     const base = fs.readFileSync('00 PCDI/base/base.pug', 'utf-8');
@@ -24,26 +25,27 @@ function buildBase(page, isChild=false) {
         fs.writeFileSync('00 PCDI/base/base.pug', base, 'utf-8');
     }
 }
+
 function compileBase() {
     let compiler = pug.compileFile('00 PCDI/base/base.pug');
     let compiledFile = compiler()
-    cleanVD()
     return compiledFile
-
 }
+
 function preCompile(file, page) {
-
+    //href's pointing to a child item
     file = file.replaceAll(`href=">index"`, `href="/${page}.html"`)
-
     let res = [...file.matchAll(/(?<=href=")>.*?(?=")/g)];
-    
     res.forEach(i=>{
         file = file.replace(i[0], `/${page}/${i[0].substring(1)}.html`)
     })
     
     return file
 }
+
 function compilePage(page) {
+    cleanVD()
+
     let pageChilds;
     let hasChildren = () => {
         if (fs.existsSync(`pages/${page}`) && fs.readdirSync(`pages/${page}`).length >= 1)
@@ -58,14 +60,19 @@ function compilePage(page) {
             let child = path.parse(item).name;
             console.log(`compiling ${page} => ${child}`)
 
-            //pre compile the child related issues into virtual dom
-            const originalFile = fs.readFileSync(`pages/${page}.pug`, 'utf-8');
-            let preCompiledFile = originalFile.replace(/include .*/, `include ../../pages/${page}/${child}`);
+            //pre compile the page related issues into virtual dom
+            const originalPage = fs.readFileSync(`pages/${page}.pug`, 'utf-8');
+            let preCompiledFile = originalPage.replace(/include .*/, `include ./${page}/${child}`);
             preCompiledFile = preCompiledFile.replaceAll('#{currentPage}', `${child}`);
             preCompiledFile = preCompile(preCompiledFile, page);
 
             fs.writeFileSync(`00 PCDI/virtualDOM/${page}.pug`, preCompiledFile, 'utf-8');
 
+            //compile the child itself
+            fs.mkdirSync(`00 PCDI/virtualDOM/${page}`, {recursive: true});
+            const originalChild = fs.readFileSync(`pages/${page}/${child}.pug`, 'utf-8')
+            let preCompiledChild = preCompile(originalChild, page);
+            fs.writeFileSync(`00 PCDI/virtualDOM/${page}/${child}.pug`, preCompiledChild, 'utf-8');
             
             //final compile and export
             if (child !== 'index') {
--- TODO.md
+++ TODO.md
@@ -6,7 +6,7 @@
 5. add safty option in all senarios possible
 6. add crash handler
 
-8. pre compile the cildern and complete the virtual dom
+
 9. config.json
 
 12. static export
